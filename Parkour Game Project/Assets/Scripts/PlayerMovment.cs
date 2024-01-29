0a9939b648ddcf0b1130b9723cf1f12724c1d6d9
Farshatacklare <123948092+Farshatacklare@users.noreply.github.com>
2024-01-29 11:43:28 +0100

    Animations v1

---
+++ Parkour Game Project/Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    private float directionX = 0f;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 14f;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        directionX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(directionX * moveSpeed, rb.velocity.y);
+       
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        UpdateAnimationUpdate();   
+    }
+
+    private void UpdateAnimationUpdate()
+    {
+        if (directionX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (directionX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
+        }
+    }
+}
