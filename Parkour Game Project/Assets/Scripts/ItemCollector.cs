837bdef2aebea438c841021b9d4cb3ec7e197c01
shrjeel12 <123947680+shrjeel12@users.noreply.github.com>
2024-02-09 14:52:46 +0100

    Score

--- Parkour Game Project/Assets/Scripts/ItemCollector.cs
+++ Parkour Game Project/Assets/Scripts/ItemCollector.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ItemCollector : MonoBehaviour
 {
     private int apple = 0;
+
+    [SerializeField] private TextMeshProUGUI appleText;
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,7 +16,7 @@ public class ItemCollector : MonoBehaviour
         {
             Destroy(collision.gameObject);
             apple++;
-            Debug.Log("Apple: " + apple);
+            appleText.text = "Apple Eaten: " + apple;
         }
 
 
